e2b3f63f7ecaeebd0eab53213ec9dec7006ce38a
Wilian Tapia <bennyrock20@hotmail.com>
2018-08-06 13:41:54 -0500

    Avances

--- sync-motora.php
+++ sync-motora.php
@@ -811,14 +811,17 @@ function syncwarehouse_write_log($message)
 /****
  Writes logs in worpdress log
  **/
-function write_log($log)
-{
-        if (is_array($log) || is_object($log)) {
-            error_log(print_r($log, true));
-        } else {
-            error_log($log);
-        }
-}
+if (!function_exists('write_log')) {
+    function write_log($log)
+    {
+            if (is_array($log) || is_object($log)) {
+                error_log(print_r($log, true));
+            } else {
+                error_log($log);
+            }
+    }
+} 
+
 
 function remote_id_admin_notice__success()
 {
